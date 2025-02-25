e95aeaa7c1de1cdd5b902c5d96dc965211ede9ec
Yashita Watchpillai <yaashirao@gmail.com>
2025-02-24 23:00:13 -0500

    latest granite model

--- args_parser.py
+++ args_parser.py
@@ -5,6 +5,6 @@ def parse_arguments():
     parser.add_argument("--task_file", type=str, required=True, help="Path to the JSON file containing task prompts")
     parser.add_argument("--initial", action="store_true", help="Evaluate only initial code")
     parser.add_argument("--refined", action="store_true", help="Evaluate only refined code")
-    parser.add_argument("--ollama_model", type=str, default="hf.co/Qiskit/granite-8b-qiskit-GGUF:latest", help="Ollama model name")
+    parser.add_argument("--transformers_model", type=str, default="Qiskit/granite-8b-qiskit-rc-0.10", help="Transformers model name")
     parser.add_argument("--gpt_model", type=str, default="o1-preview", help="GPT model name")
     return parser.parse_args()
